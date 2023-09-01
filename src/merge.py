9e29c333bab06e6e37bface6f70ba0f66197943f
modengann <99417989+modengann@users.noreply.github.com>
2023-09-01 12:02:05 -0400

    Added example

--- src/merge.py
+++ src/merge.py
@@ -4,7 +4,15 @@ def merge(L1, L2):
     pass
 
 def main():
-    pass
+    l1 = [1, 5, 2, 5, 6,1 ,2, 7, 0]
+    l1.sort()
+    print(l1)
+
+    l2 = [9, 1, 2, 6, 1, 9,6, 52]
+    l2.sort()
+    print(l2)
+
+    print(merge(l1, l2))
 
 if __name__ == "__main__":
     main()
