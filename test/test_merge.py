24af42639695a3dcba1d7c4df1715ca40fd3cff9
modengann <99417989+modengann@users.noreply.github.com>
2023-09-01 12:08:43 -0400

    Removed call test

--- test/test_merge.py
+++ test/test_merge.py
@@ -47,16 +47,6 @@ class Merge(unittest.TestCase):
         self.assertEqual(len(result), len(L), msg="Incorrect length of result list for input lists %s and %s!" % (L1, L2))
         self.assertEqual(result, L, msg="Incorrect result for input lists %s and %s!" % (L1, L2))
 
-    def test_calls(self):
-        with patch('builtins.sorted') as s:
-            merge([1,5,9,12], [2,6,10])
-            self.assertEqual(sorted.call_count, 0, msg="You weren't allowed to use function 'sorted'!")
-        # The below does not work, because list is defined in C
-        #with patch.object(list, 'sort') as sort_method:
-        with open("src/merge.py") as in_file:
-            for line in in_file:
-                self.assertFalse(".sort" in line, "You weren't allowed to use the 'sort' method")
-
 
 if __name__ == '__main__':
     unittest.main()
